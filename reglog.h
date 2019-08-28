58476f65f300ad7d5b49345cbc1467970189cf49
KasperXMS <54522666+KasperXMS@users.noreply.github.com>
2019-08-28 14:39:16 +0800

    Update reglog.h

--- reglog.h
+++ reglog.h
@@ -132,102 +132,164 @@ int regis(int choice, char path[])
 {
 	FILE *out;                                                           //设立文件指针 
 	char username[MAX], password1[MAX]={'\0'}, password2[MAX]={'\0'};    //初始化用户名密码字符串 
-	char NAME[MAX]={'\0'}, gender[10]={'\0'}, tel[MAX]={'\0'}, email[MAX]={'\0'}; 
+	char NAME[MAX]={'\0'}, gender[10]={'\0'}, tel[MAX]={'\0'}, email[MAX]={'\0'}, cinema[MAX]={'\0'}; 
 	char filename[MAX]={'\0'};  										 //初始化文件名字符串 
 	int flag=1;                                                          //控制循环 
 	printf("所有的输入不应超过20字符\n");               //“所有的输入不应超过20字符” 
-	while(flag)
+	switch(choice)
 	{
-		printf("输入用户名:");
-		scanf("%s", username);
-		printf("\n输入密码:");
-		scanf("%s", password1);
-		printf("\n再次确认密码:");
-		scanf("%s", password2);
-		printf("\n输入姓名:");
-		scanf("%s", NAME);
-		printf("\n输入性别:");
-		scanf("%s", gender);
-		printf("\n输入电话号码:");
-		scanf("%s", tel);
-		printf("\n输入电子邮箱:");
-		scanf("%s", email);
-		if(judgeUsername(username))
-		{
-			if(judgeTel(tel))
+		case 1:
+			while(flag)
 			{
-				if(judgeEmail(email))
+				printf("输入用户名:");
+				scanf("%s", username);
+				printf("\n输入密码:");
+				scanf("%s", password1);
+				printf("\n再次确认密码:");
+				scanf("%s", password2);
+				printf("\n输入姓名:");
+				scanf("%s", NAME);
+				printf("\n输入所属影院:");
+				scanf("%s", cinema);
+				printf("\n输入电子邮箱:");
+				scanf("%s", email);
+				if(judgeUsername(username))
 				{
-					flag=0;
+					if(judgeEmail(email))
+					{
+						flag=0;
+					}
+					else
+					{
+						printf("邮箱格式不正确\n");
+					}
 				}
 				else
 				{
-					printf("邮箱格式不正确\n");
+					printf("用户名格式不正确\n"); 
 				}
-			}
-			else
-			{
-				printf("电话格式不正确\n");
-			}
-		}
-		else
-		{
-			printf("用户名格式不正确\n"); 
-		}
-		 
-	}                 												 //步骤均无问题方可跳出循环 
-	
-	if(strcmp(password1,password2)==0)
-	{
-		strncpy(filename, path, strlen(path)-8);
-		if(choice==1)
-		{
-			strcat(filename, "accounts\\admin\\");
-			strcat(filename, username);
-			strcat(filename, ".acc");
-		}
-		else if(choice==2)
-		{
-			strcat(filename, "accounts\\user\\");
-			strcat(filename, username);
-			strcat(filename, ".acc");                                    //文件路径名拼接 
-		}
-		if(access(filename, 0)!=0)										 //检查用户名是否已存在 
-		{
-			if((out=fopen(filename, "w"))!=NULL)						 //新建以用户名为名称的文件并以写入模式打开 
-			{
-				fprintf(out, "%s\n", username);							  
-				fprintf(out, "%s\n", NAME);
-				fprintf(out, "%s\n", gender);
-				fprintf(out, "%s\n", tel);
-				fprintf(out, "%s\n", password2);
-				fprintf(out, "%s\n", email);
-				fprintf(out, "%lf\n", 0.00);
-				if(fclose(out)!=0)										 //防止错误关闭 
+		 		if(strcmp(password1,password2)==0)
 				{
-					fprintf(stderr, "system Error!\n");
-					return 0;
-				}
-				return 1;												 //注册成功则返回1 
-			}
-			else
-			{
-				fprintf(stderr, "System Error!\n");
-			}
+					strncpy(filename, path, strlen(path)-8);
+					strcat(filename, "accounts\\admin\\");
+					strcat(filename, username);
+					strcat(filename, ".acc");
+					if(access(filename, 0)!=0)										 //检查用户名是否已存在 
+					{
+						if((out=fopen(filename, "w"))!=NULL)						 //新建以用户名为名称的文件并以写入模式打开 
+						{
+							fprintf(out, "%s\n", username);							  
+							fprintf(out, "%s\n", cinema);
+							fprintf(out, "%s\n", NAME);
+							fprintf(out, "%s\n", email);
+							fprintf(out, "%s\n", password2);
+							if(fclose(out)!=0)										 //防止错误关闭 
+							{
+								fprintf(stderr, "system Error!\n");
+								return 0;
+							}
+							return 1;												 //注册成功则返回1 
+						}
+						else
+						{
+							fprintf(stderr, "System Error!\n");
+						}
 			
-		}
-		else
-		{
-			printf("Invalid USername!\n");
-		}
-		
-		
-	}
-	else
-	{
-		printf("Wrong password!\n"); 
-	}
-	return 0;
+					}
+					else
+					{
+						printf("Invalid USername!\n");
+					}
+				}
+				else
+				{
+					printf("Wrong password!\n"); 
+				}
+				return 0;
+			}
+			break;
+		case 2:
+			while(flag)
+			{
+				printf("输入用户名:");
+				scanf("%s", username);
+				printf("\n输入密码:");
+				scanf("%s", password1);
+				printf("\n再次确认密码:");
+				scanf("%s", password2);
+				printf("\n输入姓名:");
+				scanf("%s", NAME);
+				printf("\n输入性别:");
+				scanf("%s", gender);
+				printf("\n输入电话号码:");
+				scanf("%s", tel);
+				printf("\n输入电子邮箱:");
+				scanf("%s", email);
+				if(judgeUsername(username))
+				{
+					if(judgeTel(tel))
+					{
+						if(judgeEmail(email))
+						{
+							flag=0;
+						}
+						else
+						{
+							printf("邮箱格式不正确\n");
+						}
+					}
+					else
+					{
+						printf("电话格式不正确\n");
+					}
+				}
+				else
+				{
+					printf("用户名格式不正确\n"); 
+				}
+		 		if(strcmp(password1,password2)==0)
+				{
+					strncpy(filename, path, strlen(path)-8);
+					strcat(filename, "accounts\\user\\");
+					strcat(filename, username);
+					strcat(filename, ".acc");
+					if(access(filename, 0)!=0)										 //检查用户名是否已存在 
+					{
+						if((out=fopen(filename, "w"))!=NULL)						 //新建以用户名为名称的文件并以写入模式打开 
+						{
+							fprintf(out, "%s\n", username);							  
+							fprintf(out, "%s\n", NAME);
+							fprintf(out, "%s\n", gender);
+							fprintf(out, "%s\n", tel);
+							fprintf(out, "%s\n", password2);
+							fprintf(out, "%s\n", email);
+							fprintf(out, "%lf\n", 0.00);
+							if(fclose(out)!=0)										 //防止错误关闭 
+							{
+								fprintf(stderr, "system Error!\n");
+								return 0;
+							}
+							return 1;												 //注册成功则返回1 
+						}
+						else
+						{
+							fprintf(stderr, "System Error!\n");
+						}
+			
+					}
+					else
+					{
+						printf("Invalid USername!\n");
+					}
+				}
+				else
+				{
+					printf("Wrong password!\n"); 
+				}
+				return 0;
+			} 
+			break;
+	}	                												 //步骤均无问题方可跳出循环	
 }
 
 //-------登录机制的具体实现--------- 
@@ -236,14 +298,14 @@ char *login(int choice, char path[])
 	
 	static char username[MAX]={'\0'}, fail[5]="FAIL";
 	char password1[MAX]={'\0'}, password2[MAX]={'\0'};   //作用同上	  
-	char NAME[MAX]={'\0'}, gender[10]={'\0'}, tel[MAX]={'\0'}, email[MAX]={'\0'};
+	char NAME[MAX]={'\0'}, gender[10]={'\0'}, tel[MAX]={'\0'}, email[MAX]={'\0'}, cinema[MAX]={'\0'};
 	char filename[MAX]={'\0'};
 	char verify[7]={'\0'}, veryfin[MAX]={'\0'};
 	double remain=0.00;
 	int flag=0, value=1;
 	while(flag<4 && value)
 	{
-		FILE *in;
+		FILE *in=NULL;
 		flag=4;
 		printf("所有的输入不应超过20字符\n");
 		printf("输入用户名:");
@@ -262,48 +324,77 @@ char *login(int choice, char path[])
 		else
 		{
 			strncpy(filename, path, strlen(path)-8);
-			if(choice==1)
+			switch(choice)
 			{
-				strcat(filename, "accounts\\admin\\");
-				strcat(filename, username);
-				strcat(filename, ".acc");
-			}
-			else if(choice==2)
-			{
-				strcat(filename, "accounts\\user\\");
-				strcat(filename, username);
-				strcat(filename, ".acc");										//拼接文件路径 
-			}
-		
-			if((in=fopen(filename, "r"))!=NULL)									//试图读取用户名所指文件 
-			{
-				fscanf(in, "%s", username);							  
-				fscanf(in, "%s", NAME);
-				fscanf(in, "%s", gender);
-				fscanf(in, "%s", tel);
-				fscanf(in, "%s", password2);
-				fscanf(in, "%s", email);
-				fscanf(in, "%lf", &remain);
-				if(strcmp(password1, password2)==0)								//密码验证 
-				{
-					if(fclose(in)!=0)
+				case 1:
+					strcat(filename, "accounts\\admin\\");
+					strcat(filename, username);
+					strcat(filename, ".acc");
+					if((in=fopen(filename, "r"))!=NULL)									//试图读取用户名所指文件 
 					{
-						fprintf(stderr, "system Error!\n");
-						return fail;
-					}							 
-					return username;													//登陆成功，返回1			
-				}
-				else
-				{
-				printf("Wrong password!\n");
-				}
-			}
-			else
-			{
-				fprintf(stderr, "System Error!\n");
-			}
-			printf("Try again(1) or exit(0)?");
-			scanf("%d",&value);
+						fscanf(in, "%s", username);							  
+						fscanf(in, "%s", cinema);
+						fscanf(in, "%s", NAME);
+						fscanf(in, "%s", email);
+						fscanf(in, "%s", password2);
+						if(strcmp(password1, password2)==0)								//密码验证 
+						{
+							if(fclose(in)!=0)
+							{
+								fprintf(stderr, "system Error!\n");
+								return fail;
+							}							 
+							return username;													//登陆成功，返回1			
+						}
+						else
+						{
+							fclose(in);
+							printf("Wrong password!\n");
+						}
+					}
+					else
+					{
+						fprintf(stderr, "System Error!\n");
+					}
+					printf("Try again(1) or exit(0)?");
+					scanf("%d",&value);
+					break;
+				case 2:
+					strcat(filename, "accounts\\user\\");
+					strcat(filename, username);
+					strcat(filename, ".acc");
+					if((in=fopen(filename, "r"))!=NULL)									//试图读取用户名所指文件 
+					{
+						fscanf(in, "%s", username);							  
+						fscanf(in, "%s", NAME);
+						fscanf(in, "%s", gender);
+						fscanf(in, "%s", tel);
+						fscanf(in, "%s", password2);
+						fscanf(in, "%s", email);
+						fscanf(in, "%lf", &remain);
+						if(strcmp(password1, password2)==0)								//密码验证 
+						{
+							if(fclose(in)!=0)
+							{
+								fprintf(stderr, "system Error!\n");
+								return fail;
+							}							 
+							return username;													//登陆成功，返回1			
+						}
+						else
+						{
+							fclose(in);
+							printf("Wrong password!\n");
+						}
+					}
+					else
+					{
+						fprintf(stderr, "System Error!\n");
+					}
+					printf("Try again(1) or exit(0)?");
+					scanf("%d",&value);
+					break;
+			}	
 		}
 		if(value)
 		{
