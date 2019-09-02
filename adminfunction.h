fef6c3701290f9163ab184333bc9bfac3501b7d0
czn1007 <54526969+czn1007@users.noreply.github.com>
2019-09-02 16:29:20 +0800

    Add files via upload

--- adminfunction.h
+++ adminfunction.h
@@ -16,51 +16,7 @@ void find_studio_info(char path[]);
 void studio_info(char path[]);
 void add_session_info(char path[]);
 void session_classified(char path[]);
-void ShowSession(SessionDetail session);
-SessionDetail Read_session_to_struct(FILE *fp, SessionDetail session);
 int session_arrange(char path[], char cinema[], int newhall, int newstart, int newtime);
-
-void ShowSession(SessionDetail session)
-{
-    int i = 0;
-    printf("场次编号:%s\n", session.SessioNum);
-    printf("影片名称:%s\n", session.MovName);
-    printf("影院名称:%s\n", session.CinName);
-    printf("影厅号:%d\n", session.MovieRoom);
-    printf("影片开始时间:%s\n", session.Startime);
-    printf("影片开始时间:%s\n", session.Stoptime);
-    printf("总时长:%d\n", session.time);
-    printf("总座位:%d\n", session.AllticketNum);
-    printf("余票数:%d\n", session.remainTicket);
-    printf("票价:%lf\n", session.price);
-    printf("语言类型:%s\n", session.language);
-    printf("电影类型:%s\n", session.MovType);
-    printf("优惠政策:%f\n", session.discount);
-    printf("总排数:%d\n", session.row);
-    printf("总列数:%d\n", session.colum);
-    printf("已占座位信息:\n");
-    for (i = 0; i < session.AllticketNum - session.remainTicket; i++)
-        printf("%d排%d座\n", session.Curseat[i][0], session.Curseat[i][1]);
-}
-
-SessionDetail Read_session_to_struct(FILE *fp, SessionDetail session)
-{
-    int i = 0, seat = 0, seatx = 0, seaty = 0;
-    fscanf(fp, "%s%s%s%d", session.SessioNum, session.MovName, session.CinName, &session.MovieRoom);
-    fscanf(fp, "%s%s%d%d", session.Startime, session.Stoptime, &session.time, &session.AllticketNum);
-    fscanf(fp, "%d%lf%s%s", &session.remainTicket, &session.price, session.language, session.MovType);
-    fscanf(fp, "%f%d%d", &session.discount, &session.row, &session.colum);
-    while (!feof(fp))
-    {
-        fscanf(fp, "%d", &seat);
-        seatx = seat / 100;
-        seaty = seat % 100;
-        session.Curseat[i][0] = seatx;
-        session.Curseat[i++][1] = seaty;
-    }
-    return session;
-}
-
 void Changeinfo(char path[], char username[])
 {
     FILE *in, *out;
