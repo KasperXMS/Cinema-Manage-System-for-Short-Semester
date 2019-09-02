f4800ea319c8f757e28b753ea5db864abdd2e8f9
ITBackkom <50231687+ITBackkom@users.noreply.github.com>
2019-09-02 16:19:11 +0800

    Update session.h

--- session.h
+++ session.h
@@ -18,3 +18,27 @@ typedef struct movie{//场次信息结构体
     int seat[MAXSIZE][MAXSIZE];//座位信息
     int Curseat[MAXSIZE][2];   //已占座位信息
 }SessionDetail;
+void ShowSession(SessionDetail session);
+
+void ShowSession(SessionDetail session)
+{
+    int i = 0;
+    printf("场次编号:%s\n", session.SessioNum);
+    printf("影片名称:%s\n", session.MovName);
+    printf("影院名称:%s\n", session.CinName);
+    printf("影厅号:%d\n", session.MovieRoom);
+    printf("影片开始时间:%s\n", session.Startime);
+    printf("影片开始时间:%s\n", session.Stoptime);
+    printf("总时长:%d\n", session.time);
+    printf("总座位:%d\n", session.AllticketNum);
+    printf("余票数:%d\n", session.remainTicket);
+    printf("票价:%lf\n", session.price);
+    printf("语言类型:%s\n", session.language);
+    printf("电影类型:%s\n", session.MovType);
+    printf("优惠政策:%f\n", session.discount);
+    printf("总排数:%d\n", session.row);
+    printf("总列数:%d\n", session.colum);
+    printf("已占座位信息:\n");
+    for (i = 0; i < session.AllticketNum - session.remainTicket; i++)
+        printf("%d排%d座\n", session.Curseat[i][0], session.Curseat[i][1]);
+}
