805acc491f43aead60d70f4c6c72d481399c78cc
ITBackkom <50231687+ITBackkom@users.noreply.github.com>
2019-09-03 18:12:05 +0800

    Update session.h

--- session.h
+++ session.h
@@ -20,24 +20,20 @@ typedef struct movie{//场次信息结构体
     int Curseat[MAXSIZE][2];   //已占座位信息
 }SessionDetail;
 
+typedef struct Cinema{
+    char Ciname[5]; //影院名称
+    int MovieRoom;  //影厅号
+    int AllSeatnum;    //总座位数
+    char MovieType[6];//影片类型
+    int SelledSeat;   //已售出座位
+    int Selled_Seat_info[MAXSIZE][2];//已售出座位信息
+}Cinema;
+
+void ShowCinema(Cinema cin);
 void ShowSession(SessionDetail session);
+Cinema Read_Cinema_info_to_struct(FILE *fp,Cinema cin);
 SessionDetail Read_session_to_struct(FILE *fp, SessionDetail session);
-void ShowSession(SessionDetail session)
-{
-    int i = 0;
-    printf("+-----------------------------------------------------------------------------------------------------------------------------+\n");
-    printf("|场次编号     |影片名称      |影院名称 |影厅号 |开始时间 |结束时间 |总时长 |总座位 |余票数 |票价   |语言类型 |电影类型|优惠政策 |总排数 |总列数 |\n");
-    printf("+-----------------------------------------------------------------------------------------------------------------------------+\n");
-    printf("|%-13s|%-14s|%-9s|%-7d|%-9s|%-9s|",session.SessioNum,session.MovName,session.CinName,
-           session.MovieRoom,session.Startime,session.Stoptime);
-    printf("%-7d|%-7d|%-7d|%-7.2lf|%-9s|%-8s|%-9.2f|", session.time,session.AllticketNum,session.remainTicket,session.price,session.language,
-           session.MovType,session.discount);
-    printf("%-7d|%-7d|\n", session.row,session.colum);
-    printf("+--------------------------------------------------------------------------------------------------------------------------------------------------+\n");
-    printf("已占座位信息:\n");
-    for (i = 0; i < session.AllticketNum - session.remainTicket; i++)
-           printf("%d排%d座\n", session.Curseat[i][0], session.Curseat[i][1]);
-}
+
 SessionDetail Read_session_to_struct(FILE *fp, SessionDetail session)
 {
     int i = 0, seat = 0, seatx = 0, seaty = 0;
@@ -54,4 +50,45 @@ SessionDetail Read_session_to_struct(FILE *fp, SessionDetail session)
         session.Curseat[i++][1] = seaty;
     }
     return session;
+}
+Cinema Read_Cinema_info_to_struct(FILE *fp,Cinema cin){
+    int i = 0, seat = 0, seatx = 0, seaty = 0;
+    fscanf(fp,"%s%d%d",cin.Ciname,&cin.MovieRoom,&cin.AllSeatnum);
+    fscanf(fp,"%s%d",cin.MovieType,&cin.SelledSeat);
+    while (!feof(fp))
+    {
+        fscanf(fp, "%d", &seat);
+        seatx = seat / 100;
+        seaty = seat % 100;
+        cin.Selled_Seat_info[i][0] = seatx;
+        cin.Selled_Seat_info[i++][1] = seaty;
+    }
+    return cin;
+}
+void ShowSession(SessionDetail session)
+{
+    int i = 0;
+    printf("+-----------------------------------------------------------------------------------------------------------------------------+\n");
+    printf("|场次编号     |影片名称      |影院名称 |影厅号 |开始时间 |结束时间 |总时长 |总座位 |余票数 |票价   |语言类型 |电影类型|优惠政策 |总排数 |总列数 |\n");
+    printf("+-----------------------------------------------------------------------------------------------------------------------------+\n");
+    printf("|%-13s|%-14s|%-9s|%-7d|%-9s|%-9s|",session.SessioNum,session.MovName,session.CinName,
+           session.MovieRoom,session.Startime,session.Stoptime);
+    printf("%-7d|%-7d|%-7d|%-7.2lf|%-9s|%-8s|%-9.2f|", session.time,session.AllticketNum,session.remainTicket,session.price,session.language,
+           session.MovType,session.discount);
+    printf("%-7d|%-7d|\n", session.row,session.colum);
+    printf("+--------------------------------------------------------------------------------------------------------------------------------------------------+\n");
+    printf("已占座位信息:\n");
+    for (i = 0; i < session.AllticketNum - session.remainTicket; i++)
+           printf("%d排%d座\n", session.Curseat[i][0], session.Curseat[i][1]);
+}
+
+void ShowCinema(Cinema cin){
+    int i;
+    printf("+---------------------------------------+\n");
+    printf("|影院名称 |影厅号 |总座位 |电影类型|已售出座位数|\n");
+    printf("|%-9s|%-7d|%-7d|%-8s|%-9d|\n",cin.Ciname,cin.MovieRoom,cin.AllSeatnum,cin.MovieType,cin.SelledSeat);
+    printf("+---------------------------------------+\n");
+    printf("已占座位信息:\n");
+    for(i=0;i<cin.SelledSeat;i++)
+        printf("%d排%d座已售出\n",cin.Selled_Seat_info[i][0],cin.Selled_Seat_info[i][1]);
 }
